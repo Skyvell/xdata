caf31d17bf6c69ed757dce831627e5231d4224f3
Ted Skyvell <tskyvell@gmail.com>
2026-05-11 17:20:42 +0200

    wire Dagster+ config through live root and set dev org slug
    Cascade the new dagster_* module variables and outputs through the live
    root and pin dagster_org_slug to tedskyvell.eu in dev.tfvars.

--- infra/live/main.tf
+++ infra/live/main.tf
@@ -9,8 +9,21 @@ module "app" {
   catalog_skip_final_snapshot     = var.catalog_skip_final_snapshot
   catalog_deletion_protection     = var.catalog_deletion_protection
   catalog_allowed_cidrs           = var.catalog_allowed_cidrs
+
+  dagster_org_slug                = var.dagster_org_slug
+  dagster_deployment              = var.dagster_deployment
+  dagster_agent_image             = var.dagster_agent_image
+  dagster_agent_token_secret_name = var.dagster_agent_token_secret_name
+  dagster_agent_cpu               = var.dagster_agent_cpu
+  dagster_agent_memory            = var.dagster_agent_memory
+  dagster_agent_replicas          = var.dagster_agent_replicas
 }
 
 output "catalog_endpoint" { value = module.app.catalog_endpoint }
 output "catalog_master_secret_arn" { value = module.app.catalog_master_secret_arn }
 output "lake_bucket_name" { value = module.app.lake_bucket_name }
+output "dagster_cluster_name" { value = module.app.dagster_cluster_name }
+output "dagster_agent_role_arn" { value = module.app.dagster_agent_role_arn }
+output "dagster_run_role_arn" { value = module.app.dagster_run_role_arn }
+output "compute_role_arn" { value = module.app.compute_role_arn }
+output "dagster_url" { value = module.app.dagster_url }
