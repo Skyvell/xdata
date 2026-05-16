21bdae6e2d659acc4539aa4b8682db22f5ade743
Ted Skyvell <tskyvell@gmail.com>
2026-05-16 14:26:42 +0200

    rename ducklake_admin to metadata_admin in rds_iam grant migration

--- migrations/001_enable_rds_iam_for_admin.sql
+++ migrations/001_enable_rds_iam_for_admin.sql
@@ -1,4 +1,4 @@
--- Grant the rds_iam role to ducklake_admin so it can authenticate using
+-- Grant the rds_iam role to metadata_admin so it can authenticate using
 -- IAM-issued auth tokens (instead of a password). Paired with
 -- iam_database_authentication_enabled = true on the RDS instance.
-GRANT rds_iam TO ducklake_admin;
+GRANT rds_iam TO metadata_admin;
